4ac62b2dd69eb6d903ede87751b6dad4e5d13af2
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-02-24 16:06:54 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import { GetStaticProps } from 'next';
-
+import { Head } from 'next/document';
 import { getPrismicClient } from '../services/prismic';
-
+import Prismic from '@prismicio/client';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { format } from 'date-fns'
+import ptBR from 'date-fns/esm/locale/pt-BR/index.js';
+import Link from 'next/link';
 
-interface Post {
+type  Post = {
   uid?: string;
-  first_publication_date: string | null;
+  updatedAt: string | null;
   data: {
+    slug: string;
     title: string;
     subtitle: string;
     author: string;
   };
+}
+
+interface PostsProps  {
+  posts: Post[]
 }
 
 interface PostPagination {
@@ -23,14 +31,73 @@ interface PostPagination {
 interface HomeProps {
   postsPagination: PostPagination;
 }
+export default function Home({ posts }: Post) {
+  <>
+  <Head>
+  <title>SpaceTraveling</title>
+</Head>
+<nav>
+  <img src='Logo.svg' alt="logo" />
+</nav>
 
-// export default function Home() {
-//   // TODO
-// }
+<main className={styles.container}>
+                <div className={styles.posts}>
+                    { posts.map(post => (
+                        <Link href={`/posts/${post.slug}`}>
+                        <a key={posts.slug}href='#'>
+                        <time>{posts.updatedAt}</time>
+                        <strong>{posts.title}</strong>
+                        <p>{posts.heading}</p>
+                    </a>
+                        </Link>
+                        
+                    ))}
+                    
+                </div>
+            </main>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
 
-//   // TODO
-// };
+
+
+</>
+
+}
+
+export const getStaticProps = async () => {//TODO
+const prismic = getPrismicClient();
+
+const postsResponse = await prismic.query<any>([
+  Prismic.predicates.at('document-type', 'posts')], {
+    fetch:['posts.title', 'posts.content'],
+    pageSize: 5,
+  });//TODO
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: posts.uid,
+      title: posts.title,
+      subtitle: posts.subtitle,
+      author: posts.author,
+      banner: posts.banner,
+      heading: posts.heading,
+      body: posts.body,
+      updatedAt:format(
+        new Date(),
+        posts.last_publication_date,
+        {
+          locale: ptBR,
+        }
+      )
+       
+
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
+
+
+};
