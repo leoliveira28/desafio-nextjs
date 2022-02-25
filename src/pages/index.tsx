90d882a61b6b06c561f1b1f404f77e348d113132
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-02-25 13:37:31 -0300

    erro TypeError: Cannot read properties of undefined (reading 'title')

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,11 @@ import Prismic from '@prismicio/client';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 import { format } from 'date-fns'
-import ptBR from 'date-fns/esm/locale/pt-BR';
+import ptBR from 'date-fns/locale/pt-BR';
 import Link from 'next/link';
+import { RichText } from 'prismic-dom';
 
-type  Post = {
+interface  Post  {
   uid?: string;
   updatedAt: string | null;
   heading: string;
@@ -31,9 +32,12 @@ interface PostPagination {
 
 interface HomeProps {
   postsPagination: PostPagination;
+  posts: Post[];
 }
 export default function Home({ posts }: PostsProps) {
+        return (
   <>
+  
   <Head>
   <title>SpaceTraveling</title>
 </Head>
@@ -44,13 +48,13 @@ export default function Home({ posts }: PostsProps) {
 <main className={styles.container}>
                 <div className={styles.posts}>
                     { posts.map(post => (
-                        <Link href={`/posts/${post.data.slug}`}>
+                        //<Link href={`/posts/${post.data.slug}`}>
                         <a key={post.data.slug}href='#'>
                         <time>{post.updatedAt}</time>
                         <strong>{post.data.title}</strong>
                         <p>{post.heading}</p>
                     </a>
-                        </Link>
+                        //</Link>
                         
                     ))}
                     
@@ -61,45 +65,49 @@ export default function Home({ posts }: PostsProps) {
 
 
 </>
+        )
 
 }
 
 export const getStaticProps = async () => {//TODO
 const prismic = getPrismicClient();
 
-const postsResponse = await prismic.query<any>([
-  Prismic.predicates.at('document-type', 'posts')], {
-    fetch:['posts.title', 'posts.content'],
-    pageSize: 5,
-  });//TODO
-    console.log(postsResponse)
+const response = await prismic.query<any>([
+  Prismic.predicates.at('document.type', 'posts')], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author' ],
+    pageSize: 1,
 
-  const posts = postsResponse.results.map(post => {
+  });//TODO
+
+  const posts = response.results.map(post => {
     return {
-      slug: posts.uid,
-      title: posts.title,
-      subtitle: posts.subtitle,
-      author: posts.author,
-      banner: posts.banner,
-      heading: posts.heading,
-      body: posts.body,
-      updatedAt:format(
-        new Date(),
-        posts.last_publication_date,
-        {
-          locale: ptBR,
-        },
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+      //banner: post.data.banner.url,
+      heading: post.data.heading?.text ?? '',
+      body:  post.data.body?.text ?? '',
+      updatedAt:  format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+        locale: ptBR,
+      }
 
       )
-            
+    
     }
     
-  })
+    }
+  )
+  console.log(posts)
+
 
   return {
+    
     props: {
       posts
-    }
+    },
+    
+    revalidate: 60 * 30, // 30 minutos
     
   }
   
